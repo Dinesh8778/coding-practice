44d5934ac374141d5669a5cf2f347db5778e24d8
Dinesh Kumar S <dineshkumarselvaraj31@gmail.com>
2026-07-07 17:12:42 +0530

    Add Basic Java Solution from GeeksforGeeks

--- GeeksforGeeks/Java/Basic/Min and Max in Array/Solution.java
+++ GeeksforGeeks/Java/Basic/Min and Max in Array/Solution.java
@@ -1,16 +1,17 @@
-class Solution {
-    public ArrayList<Integer> getMinMax(int[] arr) {
-        ArrayList<Integer> ans = new ArrayList<>();
-        int max = 0, min = Integer.MAX_VALUE;
-        
-        for (int i : arr) {
-            if (max < i)
+public class Solution {
+    public List<int> getMinMax(int[] arr) {
+        List<int> list = new List<int>();
+        int max = -1, min = arr[0];
+        foreach(int i in arr) {
+            if (max < i) {
                 max = i;
-            if (min > i)
+            }
+            if (min > i) {
                 min = i;
+            }
         }
-        ans.add(min);
-        ans.add(max);
-        return ans;
+        list.Add(min);
+        list.Add(max);
+        return list;
     }
 }
