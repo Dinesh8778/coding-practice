ecaf2bde48e017d558445779a3c253c96c06dde3
Dinesh Kumar S <dineshkumarselvaraj31@gmail.com>
2026-07-14 21:24:42 +0530

    Add Easy Java Solution from LeetCode

--- LeetCode/Java/Easy/Rank Transform of an Array/Solution.java
+++ LeetCode/Java/Easy/Rank Transform of an Array/Solution.java
@@ -1,21 +1,22 @@
 class Solution {
     public int[] arrayRankTransform(int[] arr) {
 
-        int[] temp = Arrays.copyOf(arr,arr.length);
+        int[] temp = Arrays.copyOf(arr, arr.length);
         Arrays.sort(temp);
 
         int[] ans = new int[arr.length];
 
         int rank = 1;
 
-        Map<Integer,Integer> map = new HashMap();
+        Map<Integer, Integer> map = new HashMap();
 
-        for(int i : temp){
-            if(map.containsKey(i)) continue;
-            map.put(i,rank++);
+        for (int i : temp) {
+            if (map.containsKey(i))
+                continue;
+            map.put(i, rank++);
         }
 
-        for(int i = 0 ; i < arr.length ; i++){
+        for (int i = 0; i < arr.length; i++) {
             ans[i] = map.get(arr[i]);
         }
 
